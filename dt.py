f410307f45a9f0028c56dfb7ce407cdf61badbc2
jennigato <jennifer.jiang18@imperial.ac.uk>
2019-01-29 15:54:16 +0000

    more structure

--- dt.py
+++ dt.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib as plt
 
+##################################################
+#Decision Tree
+##################################################
+
 #Loading Data - should be passed as dictionary into decision tree?
 data=np.loadtxt('wifi_db/clean_dataset.txt')
 print(data)
@@ -16,16 +20,36 @@ def decision_tree_learning(data,depth):
     else:
         find_split(data)
 
-#def find_split(data):
+def find_split(data):
+    entrophy = get_entrophy(data)
 
-#def get_entrophy(data):
+def get_entrophy(data):
+    return entrophy
 
 #def split_data(data, index):
 
 #Bonus question for printing tree
 #def print_tree():
 
+##################################################
 #Evaluation
+##################################################
+
 classes = [1,2,3,4]
 
-#def confusion_matrix(test, trained, classes):
+#def evaluate(test_db, trained_tree):
+
+#def get_confusion_matrix(test, trained, classes):
+
+#def get_precision_rate(confusion_matrix):
+
+#def get_recall_rate(confusion_matrix):
+
+
+#def get_F1:
+
+#def get_avg_classification_rate:
+
+##################################################
+#Pruning
+##################################################
