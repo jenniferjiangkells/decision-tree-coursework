e6f20ef2b6ac7bb59ddcf4bc98b2b067ff459340
jennigato <jennifer.jiang18@imperial.ac.uk>
2019-01-29 12:36:04 +0000

    added some functions we should write

--- dt.py
+++ dt.py
@@ -1,6 +1,31 @@
 import numpy as np
-data1=np.loadtxt('wifi_db/clean_dataset.txt')
-print(data1)
+import matplotlib as plt
 
-data2=np.loadtxt('wifi_db/noisy_dataset.txt')
-print(data2)
+#Loading Data - should be passed as dictionary into decision tree?
+data=np.loadtxt('wifi_db/clean_dataset.txt')
+print(data)
+
+noisydata=np.loadtxt('wifi_db/noisy_dataset.txt')
+print(noisydata)
+
+#Decision tree algorithm
+def decision_tree_learning(data,depth):
+    if len(np.unique(data[:, 7])) == 1:
+        terminal_node = {'attribute': 'leaf', 'value': data[0, 7], 'left': {}, 'right': {}}
+        return terminal_node, depth
+    else:
+        find_split(data)
+
+#def find_split(data):
+
+#def get_entrophy(data):
+
+#def split_data(data, index):
+
+#Bonus question for printing tree
+#def print_tree():
+
+#Evaluation
+classes = [1,2,3,4]
+
+#def confusion_matrix(test, trained, classes):
